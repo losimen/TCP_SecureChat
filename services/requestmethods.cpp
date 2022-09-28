67106491e16f9a59703a9c23573ca008939ce349
Павло Коваль <lamens@PavlusOS.local>
2022-09-28 18:59:58 +0300

    Created RequestMethods

---
+++ services/requestmethods.cpp
@@ -0,0 +1,4 @@
+#include "requestmethods.h"
+
+const QString RequestMethods::logIn = "logIn";
+const QString RequestMethods::signUp = "signUp";
---
+++ services/requestmethods.h
@@ -0,0 +1,14 @@
+#ifndef REQUESTMETHODS_H
+#define REQUESTMETHODS_H
+
+#include <QString>
+
+
+struct RequestMethods {
+    static const QString logIn;
+    static const QString signUp;
+
+};
+
+
+#endif // REQUESTMETHODS_H
